f5c974c711f7d18449bda932e82335494b66a3f7
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-08-09 11:18:11 -0400

    Added a test that actually show parallism.

--- pytest_dask/plugin.py
+++ pytest_dask/plugin.py
@@ -4,7 +4,7 @@
 import pytest
 from _pytest.runner import CallInfo
 from dask import compute, delayed
-from distributed import Client, LocalCluster
+from distributed import Client, LocalCluster, as_completed
 from contextlib import contextmanager
 import sys
 
@@ -21,11 +21,11 @@ class DaskRunner(object):
         self.config = config
         self.scheduler_mode = config.getvalue("dask_scheduler_mode")
         remote_cluster_address = config.getvalue('dask_scheduler_address')
-        remote_cluster_address = 'tcp://127.0.0.1:8786'
+        #remote_cluster_address = 'tcp://127.0.0.1:8786'
         if remote_cluster_address:
             self.client = Client(remote_cluster_address)
         else:
-            self.cluster = LocalCluster(ip='127.0.0.1', n_workers=1, processes=True)
+            self.cluster = LocalCluster(ip='127.0.0.1', n_workers=4, processes=False)
             self.client = Client(self.cluster, set_as_default=True)
 
     def __getstate__(self):
@@ -50,50 +50,50 @@ class DaskRunner(object):
 
         def generate_tasks(session):
             for i, item in enumerate(session.items):
-                # nextitem = session.items[i + 1] if i + 1 < len(session.items) else None
 
-                @delayed
+                # @delayed(pure=False)
                 def run_test(_item):
-                    # for p in unregister_plugins:
-                    #     item.config.pluginmanager.unregister(p)
-                    new_pm = _item.config.pluginmanager.__recreate__()
-                    # item.config.__reinit__(new_pm)
-                    # TerminalReporter
-
+                    # ensure that the plugin manager gets recreated appropriately.
+                    _item.config.pluginmanager.__recreate__()
                     results = self.pytest_runtest_protocol(item=_item, nextitem=None)
-
                     return results
 
                 hook = item.ihook
                 # try to ensure that the module gets treated as a dynamic module that does not exist.
 
-                delattr(item.module, '__file__')
+                # delattr(item.module, '__file__')
 
                 setup = hook.pytest_runtest_setup
                 make_report = hook.pytest_runtest_makereport
-                fut = run_test(item)
+                fut = self.client.submit(run_test, item, pure=False)
                 yield fut
 
-        # Due to test directories being dynamic in certain cases we should make sure that our workers are using the
-        # same pythonpath that we are using here.
-        original_sys_path = self.client.run(get_imports)
-        updated_sys_path = self.client.run(update_syspath, sys.path)
-        results = compute(list(generate_tasks(session)))
+        with self.remote_syspath_ctx():
+            tasks = generate_tasks(session)
 
-        # restore correct syspath
-        for worker, value in original_sys_path.items():
-            self.client.run(restore_syspath, value, workers=[worker])
-
-        original_sys_path2 = self.client.run(get_imports)
-        assert original_sys_path == original_sys_path2
-
-        # log these reports to the console.
-        for r in results:
-            for t in r:
+            # log these reports to the console.
+            for resolved in as_completed(tasks):
+                t = resolved.result()
                 for report in t:
                     session.ihook.pytest_runtest_logreport(report=report)
 
         return True
+
+    @contextmanager
+    def remote_syspath_ctx(self):
+        # Due to test directories being dynamic in certain cases we should make sure that our workers are using the
+        # same pythonpath that we are using here.
+        original_sys_path = self.client.run(get_imports)
+        updated_sys_path = self.client.run(update_syspath, sys.path)
+        try:
+            yield
+        finally:
+            # restore correct syspath
+            for worker, value in original_sys_path.items():
+                self.client.run(restore_syspath, value, workers=[worker])
+
+            original_sys_path2 = self.client.run(get_imports)
+            assert original_sys_path == original_sys_path2
 
     def call_and_report(self, item, when, log=True, **kwds):
         call = self.call_runtest_hook(item, when, **kwds)
--- tests/test_dasktest.py
+++ tests/test_dasktest.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 
 def test_dask_flag(testdir):
@@ -20,6 +21,44 @@ def test_dask_flag(testdir):
     result.stdout.fnmatch_lines([
         '*::test_orwell PASSED',
     ])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+@pytest.mark.parametrize(x=list(range(10)))
+def simple(x):
+    assert x >= 0
+
+
+def test_larger_suite(testdir):
+    """Make sure that pytest accepts our fixture, and runs it with dask."""
+
+    # create a temporary pytest test module
+    testdir.makepyfile("""
+        import pytest
+        
+        @pytest.mark.parametrize('x', list(range(10)))
+        def test_param(x):
+            import time
+            import random
+            time.sleep(random.random() * 4)
+            assert x >= 0
+    """)
+
+    # run pytest with the following cmd args
+    result = testdir.runpytest(
+        '--dask',
+        '-v',
+    )
+
+    # fnmatch_lines does an assertion internally
+    runs = []
+    for line in result.outlines:
+        if line.startswith('test_larger_suite.py'):
+            runs.append(line)
+
+    assert runs != list(sorted(runs))
 
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
